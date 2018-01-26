f6e1f43e1e2cd2a249d3dbb8e38daa9d9904c1ed
Marwin Nortington <30412786+MarwinNortington@users.noreply.github.com>
2018-01-26 07:52:36 +0700

    Update ExerciseTwo.pas

--- ExerciseTwo.pas
+++ ExerciseTwo.pas
@@ -1,1 +1,33 @@
+//
+// Finding the max and the min in an array
+//
+// By: Marwin Nortington
+//
+
 program MaxMin;
+uses crt;
+var max,n,i: integer;
+    a: array[1..999] of integer;
+
+ begin
+
+  clrscr;
+  writeln('Enter a number: '); readln(n);
+  max:=0;
+  min:=999;
+  
+  for i:=1 to n do
+   begin
+    writeln('a[',i,']'); readln(a[i]);
+    if (a[i] > max) then
+     max:=a[i];
+    if (a[i] < max) then
+     min := a[i];
+   end;
+
+  writeln('The maximum number of the array is: ', max);
+  writeln('The minimum number of the array is: ', min);
+
+  readln;
+
+ end.
